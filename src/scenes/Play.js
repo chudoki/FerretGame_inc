b76bbdc8255db7fe12a007affdd3fa799bc024e7
SkylerHaataja <sdhaataj@ucsc.edu>
2022-05-27 11:14:38 -0700

    fix idkl

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -19,19 +19,15 @@ class Level1 extends Phaser.Scene {
         let height = 1600;
         let width = 9000;
         
-        this.playThud = false;
         
-
+        this.playThud = false;
+    
         //background music
         this.bgm =this.sound.add('sunnyMorning',{loop: true, volume:0.3});
         this.bgm.play();
+
         // input keys
-        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
-        keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
-        keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-        keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-        keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+        this.cursors = this.input.keyboard.createCursorKeys();
         
         //take in physics coordinates
         const shapes = this.cache.json.get("shapes");
@@ -42,33 +38,84 @@ class Level1 extends Phaser.Scene {
             this.matter.add.sprite(i,height-800, 'sheet', 'circle.png', {shape: shapes.circle, restitution: 1, frictionAir: .01 }).setScale(.5,.5).setDensity(.0001).setTint(99999);
 
         }
-
+        
         // camera
         this.matter.world.setBounds(0, 0,width,height-100);
         this.cameras.main.setBounds(0, 0,width,height);
-        
-        // sus is collision variable
-        var sus = this.matter.world.nextGroup(true);
-        
-        this.player = new Ferret(this, 500, height-400, 'ferret').setScale(-.25,.25).setCollisionGroup(sus);
+        //bodies
+        var Bodies = Phaser.Physics.Matter.Matter.Bodies;
+        var rect = Bodies.rectangle(0, 0, 32, 32);
+        var circleA = Bodies.circle(-70, 0, 24, { isSensor: true, label: 'left' });
+        var circleB = Bodies.circle(70, 0, 24, { isSensor: true, label: 'right' });
+        var circleC = Bodies.circle(0, -70, 24, { isSensor: true, label: 'top' });
+        var circleD = Bodies.circle(0, 70, 24, { isSensor: true, label: 'bottom' });
+
+        var compoundBody = Phaser.Physics.Matter.Matter.Body.create({
+            parts: [ rect, circleA, circleB, circleC, circleD ],
+            inertia: Infinity
+        });
+
+        this.player = this.matter.add.sprite(0, 0, 'ferret').setScale(.1);
+        this.player.setExistingBody(compoundBody);
+        this.player.setPosition(100, 300);
         
         this.player.body.sleepThreshold = -1;
         this.floor2 = this.add.rectangle(width/2,height-50,width,100,0xdda15e);
         this.cameras.main.startFollow(this.player);
 
         //collision callback
-        this.matter.world.on('collisionstart', function (event, bodyA, bodyB) {
-            console.log('collision')
+        this.matter.world.on('collisionstart', function (event) {
+            //  Loop through all of the collision pairs
+            var pairs = event.pairs;
+
+            for (var i = 0; i < pairs.length; i++)
+            {
+                var bodyA = pairs[i].bodyA;
+                var bodyB = pairs[i].bodyB;
+
+                //  We only want sensor collisions
+                if (pairs[i].isSensor)
+                {
+                    var playerBody;
+
+                    if (bodyA.isSensor)
+                    {
+                        thisplayerBody = bodyA;
+                    }
+                    else if (bodyB.isSensor)
+                    {
+                        playerBody = bodyB;
+                    }
+
+                    //  You can get to the Sprite via `gameObject` property
+                    //var playerSprite = playerBody.gameObject;
+                    //var blockSprite = blockBody.gameObject;
+                }
+            }
         });
     }
 
     update() {
-
-        this.player.update();
-        if(this.playThud == true){
-            this.sound.play('sfx_bump');
-            this.playThud = false;
+        if (this.cursors.left.isDown)
+        {
+            this.player.setVelocityX(-10);
+        }
+        else if (this.cursors.right.isDown)
+        {
+            this.player.setVelocityX(10);
+        }
+        else
+        {
+            this.player.setVelocityX(0);
+        }
+    
+        if (this.cursors.up.isDown)
+        {
+            this.player.setVelocityY(-10);
+        }
+        else if (this.cursors.down.isDown)
+        {
+            this.player.setVelocityY(10);
         }
     }
-
 }
